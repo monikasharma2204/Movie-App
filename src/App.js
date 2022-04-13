db216fa5ce42462b089e579e0b448660ce44ac68
monikasharma2204 <100335086+monikasharma2204@users.noreply.github.com>
2022-04-13 17:52:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import Main from './Components/Main';
+import './Components/style.css';
+import Card from './Components/Card';
+        function App() {
+          return(
+            <>
+            <Main/>
+            </>
+          );
+        }
+export default App;
